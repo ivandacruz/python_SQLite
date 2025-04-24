ef1aad80eef9bc414bf6fefcf6af6ee731236e89
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2025-04-24 00:20:08 -0300

    created table

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from tkinter import messagebox
 
 class SistemaDeRegistros:
     def __init__(self):
-        self.com = sqlite3.connect('estudante.db')
+        self.com = sqlite3.connect('student.db')
         self.c = self.conn.cursor()
         self.create_table()
 
     def create_table(self):
-        sefl.c.execute(''' CREATE TABLE IF NOT EXISTS estudants
-                           id INTEGER PRIMARY KEY AUTOINCREMENT,
-                           nome TEXT NOT NULL,
-                           email TEXT NOT NULL,
-                           telefone TEXT NOT NULL,
-                           genero TEXT NOT NULL,
-                           data_nascimento TEXT NOT NULL,
-                           endereco TEXT NOT NULL,
-                           curso TEXT NOT NULL,
-                           imagem TEXT NOT NULL''')
+        self.c.execute('''CREATE TABLE IF NOT EXISTS students (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name TEXT NOT NULL,
+                            e-mail TEXT NOT NULL,
+                            tel TEXT NOT NULL,
+                            sex TEXT NOT NULL,
+                            date_of_birth TEXT NOT NULL,
+                            address TEXT NOT NULL,
+                            course TEXT NOT NULL,
+                            picture TEXT NOT NULL)''')
+    def register_student(self, students):
+        self.c.execute("INSERT INTO students(name, email, tel, sex, date_of_birth, address, course, picture) VALUES(?,?,?,?,?,?,?,?)",
+                    (students))
